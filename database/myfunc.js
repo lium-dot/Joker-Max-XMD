6a7356a1a15b83012321c8294af68755e591ab87
Iconic tech ♻️ <iconictech2005@gmail.com>
2025-05-09 11:24:08 +0200

    Update myfunc.js

--- database/myfunc.js
+++ database/myfunc.js
@@ -1,10 +1,3 @@
-//base by DGXeon
-//re-upload? recode? copy code? give credit ya :)
-//YouTube: @GlobalTechInfo
-//Telegram: https://t.me/GlobalTechInc
-//GitHub: @GlobalTechInfo
-//WhatsApp: https://whatsapp.com/channel/0029VagJIAr3bbVBCpEkAM07
-//want more free bot scripts? subscribe to my youtube channel: https://youtube.com/@GlobalTechInfo
 
 const {
     proto,
